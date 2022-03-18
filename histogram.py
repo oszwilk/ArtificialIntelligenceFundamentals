f09a4647e0e8a514d00f5ae57897a4c45a46b8d5
PWilkosz99L <pwilkosz99@gmail.com>
2022-03-18 10:44:54 +0100

    Length of files in all directories

--- histogram.py
+++ histogram.py
@@ -1,5 +1,15 @@
 import os
-directory_path = 'C:/repo/ArtificialIntelligenceFundamentals/data/train/A-1'
-No_of_files = len(os.listdir(directory_path))
+directory_path = './data/train/'
 
-print(No_of_files)
+dir_names = os.listdir(directory_path)
+file_count = [None] * len(dir_names)
+
+i=0
+for name in dir_names:
+    dir = directory_path+name
+    file_count[i] = len(os.listdir(dir))
+    i+=1
+
+#test
+for nums in file_count:
+    print(nums)
