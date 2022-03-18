dca03fb668191c18ce8311a3bf10e491609a71f9
Szymon Laptop <szymongooogle@gmail.com>
2022-03-18 10:50:59 +0100

    added visualization

--- histogram.py
+++ histogram.py
@@ -1,4 +1,7 @@
 import os
+import matplotlib.pyplot as plt
+import numpy as np
+
 directory_path = './data/train/'
 
 dir_names = os.listdir(directory_path)
@@ -10,6 +13,19 @@ for name in dir_names:
     file_count[i] = len(os.listdir(dir))
     i+=1
 
-#test
-for nums in file_count:
-    print(nums)
+# --- Histogram ---
+
+x = np.arange(len(dir_names))  # the label locations
+width = 0.35  # the width of the bars
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x, file_count, width)
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Ilość')
+ax.set_title('Histogram')
+ax.set_xticks(x, dir_names)
+
+fig.tight_layout()
+
+plt.show()
