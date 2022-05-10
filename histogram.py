d6a15ed547cdc5ac040be8fc5d3da020f101d1b1
Szymon <szymon_0@hotmail.com>
2022-05-10 22:52:06 +0200

    histogram

--- histogram.py
+++ histogram.py
@@ -2,15 +2,25 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-directory_path = './data/train/'
+directory_path1 = './data/train/'
+directory_path2 = './data/valid/'
 
-dir_names = os.listdir(directory_path)
+dir_names = os.listdir(directory_path1)
 file_count = [None] * len(dir_names)
 
 i=0
 for name in dir_names:
-    dir = directory_path+name
+    dir = directory_path1+name
     file_count[i] = len(os.listdir(dir))
+    i+=1
+    
+dir_names = os.listdir(directory_path2)
+
+i=0
+
+for name in dir_names:
+    dir = directory_path2+name
+    file_count[i] = file_count[i] + len(os.listdir(dir))
     i+=1
 
 # --- Histogram ---
@@ -23,9 +33,10 @@ rects1 = ax.bar(x, file_count, width)
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
 ax.set_ylabel('Ilość')
-ax.set_title('Histogram')
+ax.set_title('Histogram ilości zdjęć dla kodu znaku')
 ax.set_xticks(x, dir_names,  rotation=90)
 
 fig.tight_layout()
 
 plt.show()
+print('end')
